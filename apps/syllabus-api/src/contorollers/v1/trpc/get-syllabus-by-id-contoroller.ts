277a1e10fe06c038011380e48602d257cc2079f2
Kuraishi Taiki <131664944+taiki-kuraishi@users.noreply.github.com>
2025-07-15 12:21:40 +0900

    ✨ Add logger to syllabus-api (#37)

--- apps/syllabus-api/src/contorollers/v1/trpc/get-syllabus-by-id-contoroller.ts
+++ apps/syllabus-api/src/contorollers/v1/trpc/get-syllabus-by-id-contoroller.ts
@@ -1,7 +1,9 @@
+import { PinoLogger } from "@src/libs/pino/logger";
 import type { Procedure } from "@src/libs/trpc/trpc";
 import type { SelectSyllabus } from "@src/models/syllabus";
 import { GetSyllabusByIdUseCase } from "@src/usecase/get-syllabus-by-id-use-case";
 import { TRPCError } from "@trpc/server";
+import type { Logger } from "pino";
 import { inject, injectable } from "tsyringe";
 import { z } from "zod";
 import { AbstractTrpcController } from "./abstract-trpc-controller";
@@ -9,9 +11,14 @@ import { AbstractTrpcController } from "./abstract-trpc-controller";
 @injectable()
 export class GetSyllabusByIdController extends AbstractTrpcController {
   public static readonly inputSchema = z.string().uuid();
+  private readonly logger: Logger;
 
-  constructor(@inject(GetSyllabusByIdUseCase) private readonly useCase: GetSyllabusByIdUseCase) {
+  constructor(
+    @inject(GetSyllabusByIdUseCase) private readonly useCase: GetSyllabusByIdUseCase,
+    @inject(PinoLogger) logger: PinoLogger,
+  ) {
     super();
+    this.logger = logger.instance.child({ module: GetSyllabusByIdController.name });
   }
 
   public buildProcedure(procedure: Procedure) {
@@ -21,6 +28,7 @@ export class GetSyllabusByIdController extends AbstractTrpcController {
         try {
           return await this.useCase.execute({ id: input });
         } catch (error) {
+          this.logger.error("Failed to get syllabus by id", { input: input, error: error });
           throw new TRPCError({
             code: "INTERNAL_SERVER_ERROR",
             message: "INTERNAL_SERVER_ERROR",
